080d57800843682f155f4f905748d7d1bd9854a1
wy8933 <wy8933@rit.edu>
2025-01-24 21:46:57 -0500

    Finished basic pathfinding

--- GGJ2025/Assets/Scripts/Enemy/BaseEnemy.cs
+++ GGJ2025/Assets/Scripts/Enemy/BaseEnemy.cs
@@ -1,6 +1,22 @@
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class BaseEnemy : MonoBehaviour
 {
-    
+    public Transform player;
+    public NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (agent != null)
+        {
+            agent.destination = player.position;
+        }
+    }
 }
